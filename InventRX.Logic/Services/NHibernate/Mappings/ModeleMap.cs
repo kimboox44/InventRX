b995aa121604e80a213691bfee5a7b2123d863cb
Nicolas <1141076@cstj.qc.ca>
2014-10-10 06:47:43 -0400

    Ajout du mapping de modèle

---
+++ InventRX.Logic/Services/NHibernate/Mappings/ModeleMap.cs
@@ -0,0 +1,32 @@
+﻿using FluentNHibernate.Mapping;
+using InventRX.Logic.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventRX.Logic.Services.NHibernate.Mappings
+{
+    public class ModeleMap : ClassMap<Modele>
+    {
+        public ModeleMap()
+        {
+            Table("Modeles");
+            LazyLoad();
+            Id(x => x.IdModele)
+                .Column("id")
+                .CustomType<int?>()
+                .Access.Property()
+                .CustomSqlType("INTEGER")
+                .Not.Nullable()
+                .GeneratedBy.Identity();
+            Map(x => x.Nom)
+                .Column("nom")
+                .CustomType<string>()
+                .Access.Property()
+                .Generated.Never()
+                .CustomSqlType("VARCHAR");
+        }
+    }
+}
